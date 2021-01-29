d60716a70fedad680792b104d1c21b5c284689fb
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2021-01-29 14:17:14 -0800

    created Home and Main screen

---
+++ src/containers/HomeScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class HomeScreen extends Component {
+  state = {
+    name: "",
+  };
+
+  onNameEntered = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hi there! Welcome to your education showcase</h1>
+        <h1>Type your name and click "Enter" below to begin!</h1>
+        <input onChange={this.onNameEntered} placeholder="Your Name" />
+        <button>Enter</button>
+      </div>
+    );
+  }
+}
+
+export default HomeScreen;
