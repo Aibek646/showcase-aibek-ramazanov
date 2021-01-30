3b9d08016cc4eb9fe433c06e9cff5000edb739f2
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2021-01-30 07:33:02 -0800

    created links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import HomeScreen from "./containers/HomeScreen";
+import { Route, Switch } from "react-router-dom";
+import MainScreen from "./containers/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <Switch>
+        <Route path="/mainscreen" component={MainScreen} />
+        <Route exact path="/" component={HomeScreen} />
+        <Route />
+      </Switch>
     </div>
   );
 }
