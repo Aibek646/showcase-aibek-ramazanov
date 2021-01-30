f0d2d43ec176e70c08ad2408b32ab44cd3f94655
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2021-01-30 10:18:45 -0800

    working on automate-input

--- src/containers/MainScreen.js
+++ src/containers/MainScreen.js
@@ -13,22 +13,49 @@ class MainScreen extends Component {
     let updatedArray = this.state.suggestion;
     if (value.length > 0) {
       const regex = new RegExp(`^${value}`, "i");
-      updatedArray = this.state.universities.sort().filter((v) => {
-        regex.test(v);
-      });
-      this.setState({
-        suggestion: updatedArray,
-        text: value,
-      });
+      updatedArray = this.state.universities
+        .sort()
+        .filter((v) => regex.test(v));
+    } else if (value.length <= 0) {
+      return this.setState({ suggestion: [] });
     }
+
+    this.setState({
+      suggestion: updatedArray,
+      text: value,
+    });
+  };
+
+  suggestionSelected = (value) => {
+    this.setState({
+      text: value,
+      suggestion: [],
+    });
+  };
+
+  renderSuggestions = () => {
+    const { suggestion } = this.state;
+    if (suggestion.length === 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {suggestion.map((univer) => (
+          <li>{univer}</li>
+        ))}
+      </ul>
+    );
   };
 
   render() {
+    const { text } = this.state;
     return (
       <div>
         <h1>Welcome {this.props.name} to educational page</h1>
         <button>Add new education</button>
-        <input />
+        <br />
+        <input type="text" value={text} onChange={this.onTextChanged} />
+        <div>{this.renderSuggestions()}</div>
       </div>
     );
   }
