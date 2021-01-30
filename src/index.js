7ce3d3b2105d84a9e88d23f69d3b0681646de987
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2021-01-30 08:05:45 -0800

    imported necessary elements from respected objects in index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createdStore, applyMiddleware, compose, combineReducers } from "redux";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import HomeScreenReducer from "./store/reducers/HomeScreen";
+import MainScreenReducer from "./store/reducers/MainScreen";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  homeScreen: HomeScreenReducer,
+  mainScreen: MainScreenReducer,
+});
+
+const store = createdStore(rootReducer, composeEnhancers);
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter store={store}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
